14347a62d77cc883209389591029ca94c7068778
nadsbordoni <nfb@cin.ufpe.br>
2021-01-13 20:04:05 -0300

    estilizando componentes com styled-components

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,8 +1,21 @@
 import React, { Component } from 'react';
+import styled from 'styled-components';
 import './App.css';
-import Radium, { StyleRoot } from 'radium';
 import Person from './Person/Person';
+//regular css
+const StyledButton = styled.button`
+  background-color: ${props => props.alt ? 'red': 'green'};
+  color: white;
+  font: inherit;
+  border: 1px solid blue;
+  padding: 8px;
+  cursor: pointer;
 
+  &:hover {
+    background-color: ${props => props.alt ? 'salmon': 'lightgreen'};
+    color: black;
+  }
+`;
 class App extends Component {
   state = { //so funciona desse jeito em componente extendido
     persons: [
@@ -16,7 +29,7 @@ class App extends Component {
 
   nameChangeHandler = (event, id) => {
     const personIndex = this.state.persons.findIndex(p => {
-      return p.id ===id;
+      return p.id === id;
 
     });
     //desse jeito muda o original
@@ -30,17 +43,17 @@ class App extends Component {
     const persons = [...this.state.persons];
     persons[personIndex] = person;
 
-    this.setState({persons: persons});
+    this.setState({ persons: persons });
   }
 
   deletePersonHandler = (personIndex) => {
-//deletar uma pessoa de uma array of person.
-//desse jeito, eu to criando um pointer e mudando o obj original. Estou mudando o dado original.
-//criar uma copia antes de manipular o objeto com slice, assim eu copio a array inteira na constante criada 
+    //deletar uma pessoa de uma array of person.
+    //desse jeito, eu to criando um pointer e mudando o obj original. Estou mudando o dado original.
+    //criar uma copia antes de manipular o objeto com slice, assim eu copio a array inteira na constante criada 
     //const persons = this.state.persons.slice(); <- jeito simples
     const persons = [...this.state.persons]; //array com os objetos da array antiga, mas é nova
-    persons.splice(personIndex,1)
-    this.setState({persons: persons})
+    persons.splice(personIndex, 1)
+    this.setState({ persons: persons })
     //just changing the pointer
   }
 
@@ -69,44 +82,45 @@ class App extends Component {
       persons = (
         <div>
           {this.state.persons.map((person, index) => {
-            return <Person 
-            click={() => this.deletePersonHandler(index)}
-            name={person.name}
-            age={person.age} 
-            key={person.id}
-            changed={(event) =>this.nameChangeHandler(event, person.id)}/>
+            return <Person
+              click={() => this.deletePersonHandler(index)}
+              name={person.name}
+              age={person.age}
+              key={person.id}
+              changed={(event) => this.nameChangeHandler(event, person.id)} />
           })}
         </div>
 
       );
-      style.backgroundColor = 'red';
-      style [':hover'] = {
-        backgroundColor: 'salmon',
-        color: 'black'
-      };
+      //style.backgroundColor = 'red';
+      //style[':hover'] = {
+      //  backgroundColor: 'salmon',
+      //  color: 'black'
+      //};
     }
     const classes = []
     if (this.state.persons.length <= 2) {
       classes.push('red'); //vermelho
     }
-    if(this.state.persons.length <= 1){
+    if (this.state.persons.length <= 1) {
       classes.push('bold'); //vermelho e negrito
     }
     return (
-      <StyleRoot>
+
       <div className="App">
         <h1>Hi, I'm a React App</h1>
-        <p className = {classes.join(' ')}>This is really working!</p>
+        <p className={classes.join(' ')}>This is really working!</p>
 
-        <button
-          style={style}
-          onClick={this.togglePersonsHandler}>Toggle Persons</button>
+        <StyledButton alt={this.state.showPersons}
+          onClick={this.togglePersonsHandler}>
+          Toggle Persons
+            </StyledButton>
         {persons}
       </div>
-      </StyleRoot>
+
     );
     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
   }
 }
 
-export default Radium(App); //higher order component
+export default App;
