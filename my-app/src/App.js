c2f60b61e4e50007e356dbf598bcd8d4d11eedea
nadsbordoni <nfb@cin.ufpe.br>
2021-01-13 16:47:27 -0300

    estilizando componentes dinamicante inline e por classe

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -50,7 +50,8 @@ class App extends Component {
   render() {
 
     const style = {
-      backgroundColor: 'white',
+      backgroundColor: 'green',
+      color: 'white',
       font: 'inherit',
       border: '1px solid blue',
       padding: '8px',
@@ -74,11 +75,19 @@ class App extends Component {
         </div>
 
       );
+      style.backgroundColor = 'red';
+    }
+    const classes = []
+    if (this.state.persons.length <= 2) {
+      classes.push('red'); //vermelho
+    }
+    if(this.state.persons.length <= 1){
+      classes.push('bold'); //vermelho e negrito
     }
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
-        <p>This is really working!</p>
+        <p className = {classes.join(' ')}>This is really working!</p>
 
         <button
           style={style}
